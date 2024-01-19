c6a277d7877299b4910cdac2e1a5236d4f75575a
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2024-01-19 18:14:38 +0530

    exam done

--- src/redux-saga/constant.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = "http://localhost:3002"
-
-export const GET_API = "/posts"
-export const POST_API = "/posts"
-export const DELETE_API = "/posts/"
-export const PUT_API = "/posts/"
